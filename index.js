70306cd4b28e73f13a00510e38855ff0a77b15b7
Maksat <makezovs@gmail.ru>
2022-12-31 01:25:03 +0300

    Webpack TZ2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import './index.css'
+import IMAGE_JS from './assets/js.2.png'
+
+const loveJs = document.createElement('h1')
+loveJs.className = 'title'
+loveJs.textContent = 'I love JavaScript'
+
+const imageHtml = document.createElement('img')
+imageHtml.className = 'js_photo'
+imageHtml.src = IMAGE_JS
+
+document.body.append(loveJs, imageHtml)
 console.log('Hello World!')
